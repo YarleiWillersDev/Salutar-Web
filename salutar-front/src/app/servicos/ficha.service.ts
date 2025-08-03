5ab1f03ebda0789ce2e4d5f3ea50210e8f27d13e
YarleiWillersDev <yarlei.willers@gmail.com>
2025-08-03 19:40:34 -0300

    Versão 1.0 Front-end

---
+++ salutar-front/src/app/servicos/ficha.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FichaPaciente } from '../model/FichaPaciente';
+import { environment } from 'src/environments/environment.development';
+import { TokenService } from './token.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FichaService {
+
+  constructor(private http: HttpClient, private tokenService: TokenService) { }
+
+  public buscarPacientes(nome: String): Observable<FichaPaciente[]> {
+    let header = this.tokenService.getTokenHeader();
+    return this.http.get<FichaPaciente[]>(environment.apiURL + "/fichas/busca?nome=" + nome, { headers: header })
+  }
+
+  public cadastrarNovaFicha(ficha: FichaPaciente): Observable<FichaPaciente> {
+       let header = this.tokenService.getTokenHeader();
+       return this.http.post<FichaPaciente>(environment.apiURL + "/fichas", ficha, { headers: header });
+  }
+
+
+  public atualizarFicha(ficha: FichaPaciente): Observable<FichaPaciente> {
+    let header = this.tokenService.getTokenHeader();
+    return this.http.put<FichaPaciente>(environment.apiURL + "/fichas/" + ficha.idFicha, ficha, { headers: header })
+  }
+
+  public buscarFichaPeloId(id: String): Observable<FichaPaciente> {
+    let header = this.tokenService.getTokenHeader();
+    return this.http.get<FichaPaciente>(environment.apiURL + "/fichas/" + id, { headers: header });
+  }
+}
